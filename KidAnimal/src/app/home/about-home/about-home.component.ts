b27ef04706ae76d4a9840e686070372b302f6be8
Brendon Reynolds <brey2615@gmail.com>
2021-11-14 01:12:10 -0500

    idt this works...

--- KidAnimal/src/app/home/about-home/about-home.component.ts
+++ KidAnimal/src/app/home/about-home/about-home.component.ts
@@ -7,8 +7,11 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class AboutHomeComponent implements OnInit {
 
-  scrollHeight:number; 
+  scrollHeight:number = 0; 
   isMobile:boolean = false; 
+  scrollDown:boolean = false;
+  scrollCount:number;
+  scrollCountArray:any[] = [];
   
   constructor() { }
 
@@ -17,25 +20,74 @@ export class AboutHomeComponent implements OnInit {
   }
 
   @HostListener('window:resize',['$event']) onResizeEvent(event):void {
-    this.isMobile = (window.innerWidth < 549) ? true : false;
+    this.isMobile = (window.innerWidth < 800) ? true : false;
   }
 
   ngOnInit(): void {
-    if(window.innerWidth < 549) { 
+    if(window.innerWidth < 800) { 
       this.isMobile = true;
     }
+    this.scrollCount = 1;
   }
 
-  titleStyle(value:number, additionalMargin:number, mobileMargin:number) { 
-    if(window.innerWidth < 549 && mobileMargin > 0) {
+  titleStyle(value:number, additionalMargin:number, tabletMargin:number, mobileMargin:number) { 
+    return;
+    // if(window.innerWidth < 549 && mobileMargin > 0) {
+    //   return { 
+    //     marginTop: `${(this.scrollHeight * - value) + mobileMargin}px`
+    //   }
+    // }
+
+    // if(window.innerWidth < 549 && tabletMargin > 0) {
+    //   return { 
+    //     marginTop: `${(this.scrollHeight * - value) + mobileMargin}px`
+    //   }
+    // }
+
+    // return { 
+    //   marginTop: `${(this.scrollHeight * - value) + additionalMargin}px`
+    // }
+  }
+
+  bootyStyle(baseMargin:number, rateOfChange:number, amountOfChange:number) {
+    let scrollTempValue;  
+    if(Number.isInteger(rateOfChange % this.scrollHeight)) {
+      if(this.scrollCountArray[1] !== undefined) {
+        scrollTempValue = this.scrollCountArray[1];
+        if((this.scrollCountArray[0] > this.scrollCountArray[1]) === true) {
+          this.scrollCount += 1;
+          this.scrollDown = true;
+        } 
+        else if((this.scrollCountArray[0] < this.scrollCountArray[1]) === true) {
+          this.scrollCount -= 1;
+          this.scrollDown = false; 
+        }  
+        this.scrollCountArray.length = 0;
+        this.scrollCountArray[0] = scrollTempValue;
+        this.scrollCountArray[1] = this.scrollHeight;
+      }
+      else { 
+        this.scrollCountArray.push(this.scrollHeight);
+      }
+      console.log('SCROLLCOUNT', this.scrollCount);
+    }
+    if(this.scrollDown) {
+      console.log('DOWN',baseMargin - (this.scrollCount + rateOfChange + amountOfChange));
       return { 
-        marginTop: `${(this.scrollHeight * - value) + mobileMargin}px`
+        marginTop: `-${baseMargin - (this.scrollCount + rateOfChange + amountOfChange)}px`
       }
     }
-
-    return { 
-      marginTop: `${(this.scrollHeight * - value) + additionalMargin}px`
-    }
+    else { 
+      console.log('UP', baseMargin - (this.scrollCount - rateOfChange - amountOfChange))
+      return { 
+        marginTop: `-${baseMargin - (this.scrollCount - rateOfChange - amountOfChange)}px`
+    } 
   }
+}
 
+    // Do we need to decrement when the user scrolls downard???
+    // else { 
+
+    // }
+    
 }
