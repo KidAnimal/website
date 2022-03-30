c486edb17985782a71d14bf5c8ac3e7b4369f67f
Brendon Reynolds <brey2615@gmail.com>
2022-03-30 17:40:37 -0400

    hero component updates

--- KidAnimal/src/app/home/hero-home/hero-home.component.ts
+++ KidAnimal/src/app/home/hero-home/hero-home.component.ts
@@ -1,31 +1,62 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChildren } from '@angular/core';
+
+// MODELS
+import { ScrollHeightElements } from 'src/app/models/scrollheight.model';
 
 @Component({
   selector: 'app-hero-home',
   templateUrl: './hero-home.component.html',
   styleUrls: ['./hero-home.component.scss']
 })
-export class HeroHomeComponent implements OnInit {
+export class HeroHomeComponent implements OnInit, AfterViewInit {
 
-  scrollHeight:number; 
-  scollPassedPoint:boolean = false;
+  scrollHeight: number;
+  height: number;
+  scollPassedPoint: boolean = false;
+  scrollElementMap: ScrollHeightElements[] = [];
 
   constructor() { }
 
-  @HostListener('window:ScrollTopHeight',['$event']) onScrollEvent(event):void {
+  @ViewChildren('scrollElement') scrollElement: QueryList<ElementRef>;
+
+  @HostListener('window:ScrollTopHeight', ['$event']) onScrollEvent(event): void {
     this.scrollHeight = event.detail;
-    if(this.scrollHeight > 100) { 
-      this.scollPassedPoint = true 
-    } 
+    if (this.scrollHeight > 100) {
+      this.scollPassedPoint = true
+    }
+
+    this.getScrollHeights();
   }
 
   ngOnInit(): void {
   }
 
-  titleStyle(value:number) { 
-    return { 
-      marginTop: `${value + (value * this.scrollHeight * -1 )}px`
-    }
+  ngAfterViewInit(): void {
+    this.scrollElement.toArray().forEach((item) => {
+      this.scrollElementMap.push({
+        nativeElement: item.nativeElement,
+        scrollTop: item.nativeElement.scrollHeight,
+        startScrollHeight: 0,
+        // travelDistance: item.nativeElement.id,
+        travelDistance: 400,
+        rateOfChange: 4
+      })
+    });
   }
 
+  getScrollHeights() {
+    this.scrollElementMap.forEach(element => {
+      // if (element.travelDistance <= this.scrollHeight && element.startScrollHeight >= this.scrollHeight) {
+        if (element.travelDistance >= this.scrollHeight) {
+          element.scrollTop = this.scrollHeight;
+          this.scrollElement.toArray().forEach((item) => {
+          this.height = element.scrollTop + element.rateOfChange;
+          item.nativeElement.style =  `margin-bottom:${this.height}px`; 
+          // if (item.nativeElement === element.nativeElement) {
+          //   console.log('item', item.nativeElement);
+          // }
+        });
+      }
+    });
+  }
 }
--- KidAnimal/src/app/models/scrollheight.model.ts
+++ KidAnimal/src/app/models/scrollheight.model.ts
@@ -9,3 +9,30 @@ export class ScrollHeight {
       this.portfolioScrollHeight = options.portfolioScrollHeight;
   }
 }
+
+export class ScrollHeightElements { 
+  nativeElement?: any; 
+  scrollTop?: number;
+  travelDistance?: number;
+  // travelDistance: TravelDistanceEnums;
+  startScrollHeight?: number;
+  rateOfChange?: number; 
+
+  constructor(options: any) { 
+    this.nativeElement = options.nativeElement;
+    this.scrollTop = options.scrollTop; 
+    this.travelDistance = options.travelDistance; 
+    this.startScrollHeight = options.startScrollHeight;
+    this.rateOfChange = options.rateOfChange; 
+
+  }
+}
+
+export enum TravelDistanceEnums { 
+  xshort = 1,
+  short = 2, 
+  medium = 3, 
+  long = 4, 
+  extraLong = 5, 
+  wtf = 6 
+} 
